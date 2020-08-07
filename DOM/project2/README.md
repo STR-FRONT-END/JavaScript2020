9ea5aaa89fa5f4138e71c2a9d938b6d1d80156e7
shareed <shanreed6@gmail.com>
2020-08-06 18:32:31 -0700

    added DOM II project

--- DOM/project2/README.md
+++ DOM/project2/README.md
@@ -1,47 +1,66 @@
-# DOM I - JavaScript Content Management System
+# DOM II - Event Exploration
 
-## Project Description
-You are going to be emulating a content managment system by controlling the content in the JavaScript file instead of the HTML file. This project is an excercise pointed at selecting elements and then updating them without touching the HTML file using the DOM.
+Fun Bus wants you to make their site more interactive. They are relying on you to provide 10 unique events to enhance their site. Explore the many events available to you by using the [MDN events reference](https://developer.mozilla.org/en-US/docs/Web/Events).
 
-You have been provided a [JSON object](js/index.js) with all the necessary data to accomplish this task.  Use dot or bracket notation to traverse the data provided.
+## Instructions
 
-**ONE RULE: You cannot update the HTML or CSS files directly.  You must use JavaScript and the DOM only for your tasks today.**
+### Task 1: Project Set Up
 
-## Set Up The Project With Git
+#### Set Up The Project With Git
 
 **Follow these steps to set up and work on your project:**
 
-* [x] Create a forked copy of this project.
-* [x] Add your project manager as collaborator on Github.
-* [x] Clone your OWN version of the repository (Not Lambda's by mistake!).
-* [x] Create a new branch: git checkout -b `<firstName-lastName>`.
-* [x] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
-* [x] Push commits: git push origin `<firstName-lastName>`.
+* [ ] Create a forked copy of this project.
+* [ ] Add your team lead as collaborator on Github.
+* [ ] Clone your OWN version of the repository (Not Lambda's by mistake!).
+* [ ] Create a new branch: git checkout -b `<firstName-lastName>`.
+* [ ] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
+* [ ] Push commits: git push origin `<firstName-lastName>`.
 
-**Follow these steps  
-for completing your project.**
+#### Launch the project with npm
 
-* [x] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's  Repo). **Please don't merge your own pull request**
-* [x] Add your project manager as a reviewer on the pull-request
-* [x] Your project manager will count the project as complete by merging the branch back into master.
+* [ ] Navigate to the root of the project with your command line.
+* [ ] Run `npm install` to download any dependencies listed in the `package.json` file.
+* [ ] Run `npm start` to compile your project and launch a development server.
+* [ ] Navigate Chrome to the URL indicated in the output of the `npm start` command.
 
-## Task 1: Create selectors to point your data into elements
-* [x] Create selectors by using any of the DOM element's methods
-* [x] Note that IDs have been used on all images.  Use the IDs to update src path content
+### Task 2a: Create listeners for 10 types of events
 
-## Task 2: Update the HTML with the JSON data
-* [x] Remember, NO direct updating of the HTML source is allowed.
-* [x] Using your selectors, update the content to match the example file.
-* [x] Remember to update the src attributes on images
+* [ ] Using your [index.js file](js/index.js), create [event listeners](https://developer.mozilla.org/en-US/docs/Web/Events) of at least 10 _different_ types. You must Use your creativity to make the Fun Bus site more interactive. For example you could change colors, animate objects, remove objects, etc. Here are some event types you could try to use:
+  * `mouseover`
+  * `keydown`
+  * `wheel`
+  * `load`
+  * `focus`
+  * `resize`
+  * `scroll`
+  * `select`
+  * `dblclick`
+  * `drag / drop`
 
-## Task 3: Add new content
-* [x] Change the color of the navigation text to be green.
-* [x] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
-* [x] Check your work by looking at the [original html](original.html) in the browser
+Note: Drag and drop is a bit more advanced than the others: it's not actually a single type of event but several types that need to work together.
 
-## Stretch Goals
-* [x] Update styles throughout the page as you see fit.  Study what happens when you updated the DOM using style in JavaScript.  
-* [x] Study tomorrow's lesson on events and try to integrate a button that can update content on the site with a click of a button.  You could build a similar data object with new values to help you test the click event.
+* [ ] Nest two similar events somewhere in the site and prevent the event propagation properly. Remember not all event types bubble.
+* [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
 
-## Stretch Project: Digital Timer
-This project is heavier on logic but employs some DOM manipulation to achieve it's goals.  Go check it out here: [stretch assignment](stretch-assignment) and see how far you can get. 
+### Task 2b: Exit Ticket
+
+Once you begin, you will have 15 minutes to answer the questions [here](https://app.codesignal.com/public-test/Xy9i8nGP4uiLbHrWT/yqgv5hmhcCLcen).
+
+The completion of these questions is mandatory for MVP. However, passing the quiz doesn't affect your standing as a Lambda School student whatsoever. This is Lambda School testing itself! Please answer honestly and to the best of your ability without using external references.
+
+### Task 3: Stretch
+
+* [ ] Go look at [GSAP](https://greensock.com/) and implement the animations found in that library with your custom events.
+
+#### Stretch assignment
+
+* [ ] Take a look at the [stretch assignment](stretch-assignment) and follow the instructions in the read me file.
+
+## Submission Format
+
+**Follow these steps for completing your project.**
+
+* [ ] Submit a Pull-Request to merge `<firstName-lastName>` Branch into `main` (student's  Repo). **Please don't merge your own pull request**
+* [ ] Add your team lead as a reviewer on the pull-request
+* [ ] Your team lead will count the project as complete by merging the branch back into `main` branch.
--- DOM/project2/js/index.js
+++ DOM/project2/js/index.js
@@ -1,141 +1,130 @@
-const siteContent = {
-    "nav": {
-      "nav-item-1": "Services",
-      "nav-item-2": "Product",
-      "nav-item-3": "Vision",
-      "nav-item-4": "Features",
-      "nav-item-5": "About",
-      "nav-item-6": "Contact",
-      "img-src": "img/logo.png"
-    },
-    "cta": {
-      "h1": "DOM Is Awesome",
-      "button": "Get Started",
-      "img-src": "img/header-img.png"
-    },
-    "main-content": {
-      "features-h4":"Features",
-      "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-      "about-h4":"About",
-      "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-      "middle-img-src": "img/mid-page-accent.jpg",
-      "services-h4":"Services",
-      "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-      "product-h4":"Product",
-      "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-      "vision-h4":"Vision",
-      "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    },
-    "contact": {
-      "contact-h4" : "Contact",
-      "address" : "123 Way 456 Street Somewhere, USA",
-      "phone" : "1 (888) 888-8888",
-      "email" : "sales@greatidea.io",
-    },
-    "footer": {
-      "copyright" : "Copyright Great Idea! 2018"
-    },
-  };
+// Your code goes here
+//1. A alert box with a message pops whenpage loads
+window.addEventListener('load', (event) => {
+  alert('THANKS FOR VISITING THE FUN BUS WEBSITE, GET READY FOR A RIDE YOU WILL NEVER FORGET');
+});
+
+//2. Background color changes when a key is pressed
+document.addEventListener('keydown', ev => {
+  ev.target.style.backgroundColor = 'black';
+  ev.target.style.color = '#FFFFFF'
+});
+
+//3.Background, border, and logo color changes on hover and 
+
+//Step 1. find the elment to put eventlistener on and store it in a variable
+const changeNavBg = document.querySelector('.main-navigation');
+//Step2.Create the function that will respond to the event
+changeNavBg.addEventListener('mouseenter',(ev) =>{
+  ev.target.style.backgroundColor = '#FEBB4E';
+  ev.target.style.borderColor = 'black';
+  ev.target.style.color = '#FFFFFF';
+});
+
+//4. Header Backgground color changes back to original color when mouse is removed
+changeNavBg.addEventListener('mouseleave',(ev) =>{
+  ev.target.style.backgroundColor = '#FFFFFF';
+  ev.target.style.color = '#212529';
+});
+
+//5. Changes the size and color of nav items on hover and 
+const changeNavTextSize = document.querySelectorAll('nav a');
+changeNavTextSize.forEach(a => {
+  a.addEventListener('mouseenter', (ev) => {
+      ev.target.style.fontSize = '3rem';
+      ev.target.style.color = '#FFFFFF';
+
+  });
+});
+
+
+//preventDeafult
+const preDefault = document.querySelectorAll('nav a');
+preDefault.forEach(a => {
+  a.addEventListener('click', (ev) => {
+      ev.target.preventDefault();
+
+  });
+});
+
+//6. Changes the size and color of navigation items back to original when mouse is moved off*/
+changeNavTextSize.forEach(a => {
+  a.addEventListener('mouseleave', (ev) => {
+      ev.target.style.fontSize = '1.6rem';
+      ev.target.style.color = '#212529';
+  });
+});
+
+
+
+
+//7. message appers when 'Lets Go' is clicked
+const messageBox = document.createElement('span');
+messageBox.textContent = 'ARE YOU READY TO JOIN THE FUN BUS';
+messageBox.style.fontSize ='3.5rem'
+messageBox.style.backgroundColor ='#FEBB4E'
+messageBox.style.borderRadius ='50%'
+messageBox.style.padding ='20px'
+messageBox.style.margin ='0 0 25px 60px'
+const message = document.querySelector('.content-section');
+message.addEventListener('click', () => {
+  message.prepend(messageBox);
+});
+
+
+
+
+const newButton = document.querySelectorAll('.btn');
+
+//8. Button color changes on hover 
+newButton.forEach(btn => {
+btn.addEventListener('mouseenter',(ev) =>{
+  ev.target.style.backgroundColor = 'red';
+  ev.target.style.color = '#212529';
+})
+});
+
+//9. Button color changes when mouse leaves
+newButton.forEach(btn => {
+btn.addEventListener('mouseleave',(ev) =>{
+  ev.target.style.backgroundColor = '#FEBB4E';
+  ev.target.style.color = '#212529';
+})
+});
+
+
+//10. Button Color changes when clicked
+newButton.forEach(btn => {
+  btn.addEventListener('click', ev =>{
+      ev.target.style.backgroundColor = 'purple';
+      ev.stopPropagation();
+  });
+});
+
+//11. The backgorund of the text(FUN IN SUN, MOUNTAIN EXCURSION, ISLAND GETAWAY) near the button changes on click
+const buttonArea = document.querySelectorAll('.destination h4');
+buttonArea.forEach(btn => {
+  btn.addEventListener('click', ev =>{
+      ev.target.style.backgroundColor = '#FEBB4E';
+      ev.target.style.color = 'black'
+      ev.target.style.borderRadius = '50%'
+      ev.target.style.fontSize = '2rem'
+      ev.target.style.padding = '10px 0 5px 50px'
+  });
+});
+
+
+
+//CODE I COULD NOT GET TO WORK
+const picBorder = document.querySelectorAll('.destination');
+picBorder.addEventListener('click', ev => {
+  ev.target.style.border = '2px solid #FEBB4E';
   
+});
+
+const newFooter = document.querySelector('footer');
+newFooter.addEventListener('click',(ev) =>{
+  ev.target.style.backgroundColor = '#FFFFFF';
+  ev.target.style.color = '#212529';
   
-  
-  
-  // Example: Update the img src for the logo
-  let logo = document.getElementById("logo-img");
-  logo.setAttribute('src', siteContent["nav"]["img-src"])
-  
-  //Updating Nav items
-  let navItems = document.getElementsByTagName('a');
-  navItems[0].textContent = siteContent["nav"]["nav-item-1"];
-  navItems[1].textContent = siteContent["nav"]["nav-item-2"];
-  navItems[2].textContent = siteContent["nav"]["nav-item-3"];
-  navItems[3].textContent = siteContent["nav"]["nav-item-4"];
-  navItems[4].textContent = siteContent["nav"]["nav-item-5"];
-  navItems[5].textContent = siteContent["nav"]["nav-item-6"];
-  
-  //Stretch
-  
-  let greenNav = document.querySelectorAll('a');
-  greenNav[0].style.color = "green";
-  greenNav[1].style.color = "green";
-  greenNav[2].style.color = "green";
-  greenNav[3].style.color = "green";
-  greenNav[4].style.color = "green";
-  greenNav[5].style.color = "green";
-  
-  let newNav = document.querySelector("nav");
-  let newNavItem = document.createTextNode('Im New');
-  let newNavItem2 = document.createTextNode('Im New Too');
-  newNav.appendChild(newNavItem2);
-  newNav.prepend(newNavItem);
-  newNav.style.color = 'green';
-  
-  
-  
-  
-  //Updating cta-text...no way I could do this using the cta object
-  let h1Tag = document.querySelector('h1');
-  let lineBreak = document.createElement('br');
-  let lineBreak2 = document.createElement('br');
-  let isText = document.createTextNode('Is');
-  let awesomeText = document.createTextNode('Awesome');
-  h1Tag.textContent = 'DOM';
-  h1Tag.appendChild(lineBreak);
-  h1Tag.appendChild(isText);
-  h1Tag.appendChild(lineBreak2);
-  h1Tag.appendChild(awesomeText);
-  
-  
-  //button
-  let button = document.querySelector('button');
-  button.textContent = siteContent["cta"]["button"];
-  
-  //header image
-  let headerImage = document.getElementById("cta-img");
-  headerImage.setAttribute('src', siteContent["cta"]["img-src"])
-  
-  
-  //Top-Content
-  let h4Tag = document.getElementsByTagName('h4');
-  h4Tag[0].textContent = siteContent["main-content"]["features-h4"]
-  h4Tag[1].textContent = siteContent["main-content"]["about-h4"]
-  h4Tag[2].textContent = siteContent["main-content"]["services-h4"]
-  h4Tag[3].textContent = siteContent["main-content"]["product-h4"]
-  h4Tag[4].textContent = siteContent["main-content"]["vision-h4"]
-  h4Tag[5].textContent = siteContent["contact"]["contact-h4"]
-  //Top P
-  let pTags = document.getElementsByTagName('p');
-  pTags[0].textContent = siteContent["main-content"]["features-content"];
-  pTags[1].textContent = siteContent["main-content"]["about-content"];
-  
-  //Image
-  let middleImage = document.getElementById("middle-img");
-  middleImage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
-  
-  pTags[2].textContent = siteContent["main-content"]["services-content"];
-  pTags[3].textContent = siteContent["main-content"]["product-content"];
-  pTags[4].textContent = siteContent["main-content"]["vision-content"];
-  
-  //Contact Section
-  let lineBreak3 = document.createElement('br');
-  let addressText = document.createTextNode('Somewhere, USA');
-  pTags[5].textContent = '123 Way 456 Street';
-  pTags[5].appendChild(lineBreak3);
-  pTags[5].appendChild(addressText);
-  pTags[6].textContent = siteContent["contact"]["phone"];
-  pTags[7].textContent = siteContent["contact"]["email"];
-  
-  //Footer
-  pTags[8].textContent = siteContent["footer"]["copyright"];
-  //
-  
-  
-  
-  
-  
-  
-  //Strecth
-  //document.body.style.backgroundColor = 'yellow'
-  
-  
-  
+});
